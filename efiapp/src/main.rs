b6132d73d189880d0690317e3f7b2bcede387f38
Noam Raz <noamraz8@gmail.com>
2021-09-14 19:32:01 +0300

    Slightly reorganize main

--- efiapp/src/main.rs
+++ efiapp/src/main.rs
@@ -31,79 +31,76 @@ fn handle_panic(_info: &PanicInfo) -> ! {
     halt()
 }
 
-fn run(image_handle: Handle, boot_table: BootTableHandle) -> Result<()> {
-    allocator::with(&boot_table, || {
-        let boot_services = boot_table.boot_services();
-        let mut stdout = boot_table.stdout();
+fn run(image_handle: Handle, boot_table: &BootTableHandle) -> Result<()> {
+    let boot_services = boot_table.boot_services();
+    let mut stdout = boot_table.stdout();
 
-        stdout.reset()?;
-        writeln!(
-            stdout,
-            "Firmware vendor: {}\nFirmware revision: {}\n",
-            boot_table.firmware_vendor(),
-            boot_table.firmware_revision()
-        )
-        .unwrap();
+    stdout.reset()?;
+    writeln!(
+        stdout,
+        "Firmware vendor: {}\nFirmware revision: {}\n",
+        boot_table.firmware_vendor(),
+        boot_table.firmware_revision()
+    )
+    .unwrap();
 
-        let loaded_image =
-            boot_services.open_protocol::<LoadedImage>(image_handle, image_handle)?;
+    let loaded_image = boot_services.open_protocol::<LoadedImage>(image_handle, image_handle)?;
 
-        writeln!(
-            stdout,
-            "Loaded image: base {:?}, size {}",
-            loaded_image.image_base(),
-            loaded_image.image_size(),
-        )
-        .unwrap();
+    writeln!(
+        stdout,
+        "Loaded image: base {:?}, size {}",
+        loaded_image.image_base(),
+        loaded_image.image_size(),
+    )
+    .unwrap();
 
-        let device_path = loaded_image.file_path();
-        let path_to_text = boot_services.locate_protocol::<DevicePathToText>()?;
+    let device_path = loaded_image.file_path();
+    let path_to_text = boot_services.locate_protocol::<DevicePathToText>()?;
 
-        let path = {
-            let raw = path_to_text.device_path_to_text(&device_path, true, true)?;
-            unsafe { Box::from_raw(raw.as_ptr()) }
+    let path = {
+        let raw = path_to_text.device_path_to_text(&device_path, true, true)?;
+        unsafe { Box::from_raw(raw.as_ptr()) }
+    };
+
+    writeln!(stdout, "Image path: {}\n", path).unwrap();
+
+    writeln!(stdout, "Path nodes:").unwrap();
+    for device_node in device_path.nodes() {
+        let node = unsafe {
+            Box::from_raw(
+                path_to_text
+                    .device_node_to_text(device_node, true, true)?
+                    .as_ptr(),
+            )
         };
 
-        writeln!(stdout, "Image path: {}\n", path).unwrap();
+        writeln!(stdout, "{}", node).unwrap();
+    }
+    writeln!(stdout).unwrap();
 
-        writeln!(stdout, "Path nodes:").unwrap();
-        for device_node in device_path.nodes() {
-            let node = unsafe {
-                Box::from_raw(
-                    path_to_text
-                        .device_node_to_text(device_node, true, true)?
-                        .as_ptr(),
-                )
-            };
+    let mmap_size = boot_services.memory_map_size()? + 0x100;
+    let mut mmap_buf = vec![0u8; mmap_size];
 
-            writeln!(stdout, "{}", node).unwrap();
-        }
-        writeln!(stdout).unwrap();
+    let (_key, mmap) = boot_services.memory_map(&mut mmap_buf)?;
 
-        let mmap_size = boot_services.memory_map_size()? + 0x100;
-        let mut mmap_buf = vec![0u8; mmap_size];
+    let conventional_mem_pages: u64 = mmap
+        .filter(|desc| desc.mem_type == MemoryType::CONVENTIONAL)
+        .map(|desc| desc.page_count)
+        .sum();
 
-        let (_key, mmap) = boot_services.memory_map(&mut mmap_buf)?;
+    writeln!(
+        stdout,
+        "Free memory: {} pages (~{}MB)",
+        conventional_mem_pages,
+        (conventional_mem_pages * 0x1000) / 0x100000
+    )
+    .unwrap();
 
-        let conventional_mem_pages: u64 = mmap
-            .filter(|desc| desc.mem_type == MemoryType::CONVENTIONAL)
-            .map(|desc| desc.page_count)
-            .sum();
-
-        writeln!(
-            stdout,
-            "Free memory: {} pages (~{}MB)",
-            conventional_mem_pages,
-            (conventional_mem_pages * 0x1000) / 0x100000
-        )
-        .unwrap();
-
-        Ok(())
-    })
+    Ok(())
 }
 
 #[no_mangle]
 pub extern "efiapi" fn efi_main(image_handle: Handle, boot_table: BootTableHandle) -> Status {
-    let _ = run(image_handle, boot_table);
+    let _ = allocator::with(&boot_table, || run(image_handle, &boot_table));
     halt();
 }
