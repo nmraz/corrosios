ad206b110ec5ea2f272cd5c19974ac452fd9592b
Noam Raz <noamraz8@gmail.com>
2021-09-24 16:46:46 +0300

    Reorganize hosttools

--- hosttools/src/lib.rs
+++ hosttools/src/lib.rs
@@ -1,13 +1,14 @@
 use std::collections::BTreeMap;
 use std::convert::TryFrom;
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 use std::io::{self, Write};
 use std::path::{Path, PathBuf};
-use std::process::{Command, ExitStatus};
+use std::process::Command;
 
 use anyhow::{anyhow, bail, Context, Result};
 use cargo_metadata::Message;
 use fatfs::{FileSystem, FormatVolumeOptions, FsOptions, ReadWriteSeek};
+use fscommon::StreamSlice;
 use gpt::disk::LogicalBlockSize;
 use gpt::mbr::ProtectiveMBR;
 use gpt::{GptConfig, GptDisk};
@@ -18,19 +19,62 @@ const MB: u64 = KB * KB;
 const LB_SIZE: u64 = 512;
 
 const EFI_PARTITION_SIZE: u64 = 10 * MB;
-pub const DISK_SIZE: u64 = EFI_PARTITION_SIZE + 64 * KB;
+const DISK_SIZE: u64 = EFI_PARTITION_SIZE + 64 * KB;
 
-pub fn cargo_cross_freestanding(
+const EFI_PACKAGE_NAME: &str = "efiapp";
+const EFI_PACKAGE_TARGET: &str = "x86_64-unknown-uefi";
+
+pub fn create_disk_image(additional_args: &[String]) -> Result<PathBuf> {
+    cross_run_all("build", additional_args)?;
+    let binary = efi_binary_path(additional_args)?;
+
+    let image_path = binary.with_extension("img");
+
+    let mut disk = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(&image_path)?;
+    disk.set_len(DISK_SIZE)?;
+
+    let mut gdisk = format_gpt(&mut disk).context("failed to format GPT disk")?;
+    let (start, end) = add_efi_partition(&mut gdisk)?;
+    gdisk.write().context("failed to flush partition table")?;
+
+    let efi_part_data = StreamSlice::new(disk, start, end)?;
+    format_efi_partition(efi_part_data, &binary).context("failed to write EFI system partition")?;
+
+    Ok(image_path)
+}
+
+pub fn cross_run_all(subcommand: &str, additional_args: &[String]) -> Result<()> {
+    cross_run(
+        subcommand,
+        EFI_PACKAGE_NAME,
+        EFI_PACKAGE_TARGET,
+        additional_args,
+    )
+}
+
+fn efi_binary_path(additional_args: &[String]) -> Result<PathBuf> {
+    built_binary_path(EFI_PACKAGE_NAME, EFI_PACKAGE_TARGET, additional_args)
+}
+
+fn cross_run(
     subcommand: &str,
     package_name: &str,
     target: &str,
     additional_args: &[String],
-) -> Result<ExitStatus> {
+) -> Result<()> {
     let mut cmd = freestanding_cross_cmd(subcommand, package_name, target, additional_args);
-    Ok(cmd.status()?)
+    if !cmd.status()?.success() {
+        bail!("`cargo {}` failed", subcommand);
+    }
+    Ok(())
 }
 
-pub fn built_binary_path(
+fn built_binary_path(
     package_name: &str,
     target: &str,
     additional_args: &[String],
@@ -72,7 +116,7 @@ fn freestanding_cross_cmd(
     cmd
 }
 
-pub fn format_gpt(disk: &mut File) -> Result<GptDisk<'_>> {
+fn format_gpt(disk: &mut File) -> Result<GptDisk<'_>> {
     let mbr =
         ProtectiveMBR::with_lb_size(u32::try_from(DISK_SIZE / LB_SIZE - 1).unwrap_or(0xffffffff));
     mbr.overwrite_lba0(disk).context("failed to write MBR")?;
@@ -90,7 +134,7 @@ pub fn format_gpt(disk: &mut File) -> Result<GptDisk<'_>> {
     Ok(gdisk)
 }
 
-pub fn add_efi_partition(gdisk: &mut GptDisk<'_>) -> Result<(u64, u64)> {
+fn add_efi_partition(gdisk: &mut GptDisk<'_>) -> Result<(u64, u64)> {
     let id = gdisk
         .add_partition(
             "EFI System Partition",
@@ -111,7 +155,7 @@ pub fn add_efi_partition(gdisk: &mut GptDisk<'_>) -> Result<(u64, u64)> {
     Ok((start, end))
 }
 
-pub fn format_efi_partition(mut partition: impl ReadWriteSeek, efi_binary: &Path) -> Result<()> {
+fn format_efi_partition(mut partition: impl ReadWriteSeek, efi_binary: &Path) -> Result<()> {
     fatfs::format_volume(&mut partition, FormatVolumeOptions::new())?;
     let fs = FileSystem::new(partition, FsOptions::new())?;
 
--- hosttools/src/main.rs
+++ hosttools/src/main.rs
@@ -1,17 +1,9 @@
-use std::fs::OpenOptions;
-use std::path::PathBuf;
 use std::process::Command;
 
-use anyhow::{bail, Context, Result};
+use anyhow::{Context, Result};
 use argh::FromArgs;
-use fscommon::StreamSlice;
-use hosttools::{
-    add_efi_partition, built_binary_path, cargo_cross_freestanding, format_efi_partition,
-    format_gpt, DISK_SIZE,
-};
 
-const EFI_PACKAGE_NAME: &str = "efiapp";
-const EFI_PACKAGE_TARGET: &str = "x86_64-unknown-uefi";
+use hosttools::{create_disk_image, cross_run_all};
 
 #[derive(FromArgs)]
 /// Tools for use on the host.
@@ -63,14 +55,14 @@ fn main() -> Result<()> {
     let args: Args = argh::from_env();
 
     match &args.subcommand {
-        Subcommand::Cross(build) => cross_efi_app(&build.subcommand, &build.additional_args),
+        Subcommand::Cross(cross) => cross_run_all(&cross.subcommand, &cross.additional_args),
         Subcommand::Image(image) => {
-            let image_path = create_uefi_image(&image.additional_build_args)?;
+            let image_path = create_disk_image(&image.additional_build_args)?;
             println!("Created UEFI image: {}", image_path.display());
             Ok(())
         }
         Subcommand::Qemu(qemu) => {
-            let image_path = create_uefi_image(&qemu.additional_build_args)?;
+            let image_path = create_disk_image(&qemu.additional_build_args)?;
             let mut cmd = Command::new("qemu-system-x86_64");
             cmd.args([
                 "-bios",
@@ -82,45 +74,4 @@ fn main() -> Result<()> {
             Ok(())
         }
     }
-}
-
-fn create_uefi_image(additional_args: &[String]) -> Result<PathBuf> {
-    cross_efi_app("build", additional_args)?;
-    let binary = efi_binary_path(additional_args)?;
-
-    let image_path = binary.with_extension("img");
-
-    let mut disk = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open(&image_path)?;
-    disk.set_len(DISK_SIZE)?;
-
-    let mut gdisk = format_gpt(&mut disk).context("failed to format GPT disk")?;
-    let (start, end) = add_efi_partition(&mut gdisk)?;
-    gdisk.write().context("failed to flush partition table")?;
-
-    let efi_part_data = StreamSlice::new(disk, start, end)?;
-    format_efi_partition(efi_part_data, &binary).context("failed to write EFI system partition")?;
-
-    Ok(image_path)
-}
-
-fn cross_efi_app(subcommand: &str, additional_args: &[String]) -> Result<()> {
-    let status = cargo_cross_freestanding(
-        subcommand,
-        EFI_PACKAGE_NAME,
-        EFI_PACKAGE_TARGET,
-        additional_args,
-    )?;
-    if !status.success() {
-        bail!("`cargo {}` failed", subcommand);
-    }
-    Ok(())
-}
-
-fn efi_binary_path(additional_args: &[String]) -> Result<PathBuf> {
-    built_binary_path(EFI_PACKAGE_NAME, EFI_PACKAGE_TARGET, additional_args)
 }
