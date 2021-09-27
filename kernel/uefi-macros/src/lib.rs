bfeb60f039b3d06e9da35baff73b6d8608d3db1c
Noam Raz <noamraz8@gmail.com>
2021-09-27 19:13:40 +0300

    Create `u16cstr` proc macro

--- kernel/uefi-macros/src/lib.rs
+++ kernel/uefi-macros/src/lib.rs
@@ -1,1 +1,31 @@
+extern crate proc_macro;
 
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, LitStr};
+
+#[proc_macro]
+pub fn u16cstr(input: TokenStream) -> TokenStream {
+    let lit = parse_macro_input!(input as LitStr).value();
+    let mut encoded = Vec::with_capacity(lit.len() + 1);
+
+    ucs2::encode_with(&lit, |c| {
+        if c == 0 {
+            panic!("embedded nul in `U16CStr` literal");
+        }
+
+        encoded.push(c);
+
+        Ok(())
+    })
+    .expect("invalid UCS-2 in `U16CStr` literal");
+
+    let expanded = quote! {
+        unsafe {
+            ::uefi::U16CStr::from_u16s_with_nul_unchecked(&[
+                #(#encoded),*, 0
+            ])
+        }
+    };
+    expanded.into()
+}
--- kernel/uefi/src/lib.rs
+++ kernel/uefi/src/lib.rs
@@ -2,6 +2,8 @@
 #![feature(allocator_api)]
 #![no_std]
 
+pub use uefi_macros::u16cstr;
+
 pub use bootalloc::BootAlloc;
 pub use cstr::*;
 pub use status::{Result, Status};
