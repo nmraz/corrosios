5ac688d915c95dcf6b15f12d5a943a10869af03e
Noam Raz <noamraz8@gmail.com>
2021-10-10 12:08:20 +0300

    Define boot info header

--- kernel/bootinfo/src/lib.rs
+++ kernel/bootinfo/src/lib.rs
@@ -1,2 +1,18 @@
 #![no_std]
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct ItemKind(pub u32);
+
+impl ItemKind {
+    pub const CONTAINER: Self = Self(0xb007b081);
+}
+
+pub const ITEM_ALIGN: usize = 8;
+
+#[derive(Debug, Clone, Copy)]
+#[repr(C)]
+pub struct ItemHeader {
+    pub kind: ItemKind,
+    pub payload_len: u32,
+}
