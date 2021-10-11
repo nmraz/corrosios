4e618fe602686824c4c9c83abf1978d35c02321e
Noam Raz <noamraz8@gmail.com>
2021-10-11 10:40:52 +0300

    Create bootinfo memory range descriptors

--- kernel/bootinfo/src/lib.rs
+++ kernel/bootinfo/src/lib.rs
@@ -26,3 +26,23 @@ pub struct ItemHeader {
     pub kind: ItemKind,
     pub payload_len: u32,
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct MemoryKind(pub u32);
+
+impl MemoryKind {
+    pub const RESERVED: Self = Self(0);
+    pub const USABLE: Self = Self(1);
+    pub const FIRMWARE: Self = Self(2);
+    pub const ACPI_TABLES: Self = Self(3);
+    pub const UNUSABLE: Self = Self(4);
+}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(C)]
+pub struct MemoryRange {
+    pub start_page: usize,
+    pub page_count: usize,
+    pub kind: MemoryKind,
+}
