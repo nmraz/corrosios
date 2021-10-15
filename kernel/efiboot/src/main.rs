5e81c5543275efdec8bf0498706678bac93827e4
Noam Raz <noamraz8@gmail.com>
2021-10-16 02:09:03 +0300

    Implement basic bootloader

--- kernel/efiboot/src/main.rs
+++ kernel/efiboot/src/main.rs
@@ -6,24 +6,23 @@
 
 extern crate alloc;
 
-use alloc::boxed::Box;
-use alloc::vec;
 use core::fmt::Write;
+use core::mem;
 use core::panic::PanicInfo;
 
-use uefi::proto::fs::{OpenMode, SimpleFileSystem};
-use uefi::proto::image::LoadedImage;
-use uefi::proto::io::SimpleTextOutput;
-use uefi::proto::path::DevicePathToText;
-use uefi::proto::ProtocolHandle;
-use uefi::table::{BootServices, BootTable, OpenProtocolHandle};
-use uefi::{u16cstr, Handle, MemoryType, Result, Status};
-
 use uninit::extension_traits::AsOut;
+
+use bootinfo::builder::Builder;
+use bootinfo::{ItemKind, MemoryKind, MemoryRange};
+use uefi::table::BootTable;
+use uefi::{Handle, MemoryDescriptor, MemoryType, Result, Status};
+
+use page::PAGE_SIZE;
 
 mod allocator;
 mod elfload;
 mod page;
+mod setup;
 
 fn halt() -> ! {
     unsafe {
@@ -41,121 +40,71 @@ fn handle_panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "efiapi" fn efi_main(image_handle: Handle, boot_table: BootTable) -> Status {
-    let res = allocator::with(&boot_table, || run(image_handle, &boot_table));
-
-    if let Err(status) = res {
-        writeln!(boot_table.stdout(), "Error: {:#x}", status.0).unwrap();
-    }
-
-    halt();
+    run(image_handle, boot_table)
+        .err()
+        .unwrap_or(Status::LOAD_ERROR)
 }
 
-fn run(image_handle: Handle, boot_table: &BootTable) -> Result<()> {
-    let boot_services = boot_table.boot_services();
-    let mut stdout = boot_table.stdout();
+fn run(image_handle: Handle, boot_table: BootTable) -> Result<()> {
+    let ctx = allocator::with(&boot_table, || setup::setup(image_handle, &boot_table))?;
 
-    stdout.reset()?;
+    boot_table.stdout().reset()?;
     writeln!(
-        stdout,
-        "Firmware vendor: {}\nFirmware revision: {}\n",
-        boot_table.firmware_vendor(),
-        boot_table.firmware_revision()
+        boot_table.stdout(),
+        "Kernel entry: {:#x}\nMemmap size: {:#x}",
+        ctx.kernel_entry,
+        ctx.mmap_buf.len()
     )
     .unwrap();
 
-    let loaded_image = boot_services.open_protocol::<LoadedImage>(image_handle, image_handle)?;
+    let (runtime_table, mmap) =
+        boot_table.exit_boot_services(image_handle, ctx.mmap_buf.as_out())?;
 
-    print_bootloader_info(boot_services, &mut stdout, &loaded_image)?;
-
-    let kentry = load_kernel(boot_services, image_handle, &loaded_image)?;
-    writeln!(stdout, "Loaded kernel, entry point: {:#x}\n", kentry).unwrap();
-
-    print_mem_map(boot_services, &mut stdout)?;
-
-    Ok(())
-}
-
-fn print_bootloader_info(
-    boot_services: &BootServices,
-    stdout: &mut ProtocolHandle<'_, SimpleTextOutput>,
-    loaded_image: &OpenProtocolHandle<'_, LoadedImage>,
-) -> Result<()> {
-    let device_path = loaded_image.file_path();
-    let path_to_text = boot_services.locate_protocol::<DevicePathToText>()?;
-
-    let path = {
-        let raw = path_to_text.device_path_to_text(&device_path, true, true)?;
-        unsafe { Box::from_raw(raw.as_ptr()) }
-    };
-
-    writeln!(stdout, "Bootloader path: {}\n", path).unwrap();
-
-    Ok(())
-}
-
-fn load_kernel(
-    boot_services: &BootServices,
-    image_handle: Handle,
-    loaded_image: &OpenProtocolHandle<'_, LoadedImage>,
-) -> Result<u64> {
-    let boot_fs = boot_services
-        .open_protocol::<SimpleFileSystem>(loaded_image.device_handle(), image_handle)?;
-
-    let root_dir = boot_fs.open_volume()?;
-    let mut file = root_dir.open(u16cstr!("regasos\\kernel"), OpenMode::READ)?;
-
-    elfload::load_elf(boot_services, &mut file)
-}
-
-fn print_mem_map(
-    boot_services: &BootServices,
-    stdout: &mut ProtocolHandle<'_, SimpleTextOutput>,
-) -> Result<()> {
-    let mmap_size = boot_services.memory_map_size()? + 0x100;
-    let mut mmap_buf = vec![0; mmap_size];
-
-    let (_key, mmap) = boot_services.memory_map(mmap_buf.as_out())?;
-
-    let conventional_mem_pages: u64 = mmap
-        .clone()
-        .filter(|desc| desc.mem_type == MemoryType::CONVENTIONAL)
-        .map(|desc| desc.page_count)
-        .sum();
-
-    writeln!(
-        stdout,
-        "Free memory: {} pages (~{}MB)\n",
-        conventional_mem_pages,
-        (conventional_mem_pages * 0x1000) / 0x100000
-    )
-    .unwrap();
-
-    let low_mem = mmap.filter(|desc| desc.phys_start < 0x1000000);
-
-    writeln!(stdout, "Low memory map:").unwrap();
-    for desc in low_mem {
-        let mem_type = match desc.mem_type {
-            MemoryType::BOOT_SERVICES_CODE => "boot services code",
-            MemoryType::BOOT_SERVICES_DATA => "boot services data",
-            MemoryType::RUNTIME_SERVICES_CODE => "runtime services code",
-            MemoryType::RUNTIME_SERVICES_DATA => "runtime services data",
-            MemoryType::LOADER_CODE => "loader code",
-            MemoryType::LOADER_DATA => "loader data",
-            MemoryType::CONVENTIONAL => "usable",
-            MemoryType::RESERVED => "reserved",
-            MemoryType::UNUSABLE => "unusable",
-            _ => "other",
-        };
-
-        writeln!(
-            stdout,
-            "{:#x}-{:#x}: {}",
-            desc.phys_start,
-            desc.phys_start + desc.page_count * 0x1000,
-            mem_type
-        )
+    let mut builder = ctx.bootinfo_builder;
+    builder
+        .append(ItemKind::EFI_SYSTEM_TABLE, runtime_table)
         .unwrap();
-    }
 
-    Ok(())
+    append_mmap(&mut builder, mmap);
+
+    let bootinfo_header = builder.finish();
+    let entry: extern "sysv64" fn(usize) -> ! = unsafe { mem::transmute(ctx.kernel_entry) };
+
+    entry(bootinfo_header as *const _ as usize);
+}
+
+fn append_mmap<'a>(
+    builder: &mut Builder,
+    mmap: impl ExactSizeIterator<Item = &'a MemoryDescriptor>,
+) {
+    // Safety: the loop below initializes all `mmap.len()` elements.
+    let buf = unsafe { builder.reserve(ItemKind::MEMORY_MAP, mmap.len()) }.unwrap();
+
+    for (efi_desc, info) in mmap.zip(buf) {
+        info.write(MemoryRange {
+            start_page: efi_desc.page_count as usize / PAGE_SIZE,
+            page_count: efi_desc.page_count as usize,
+            kind: mem_kind_from_efi(efi_desc.mem_type),
+        });
+    }
+}
+
+fn mem_kind_from_efi(efi_type: MemoryType) -> MemoryKind {
+    match efi_type {
+        MemoryType::CONVENTIONAL
+        | MemoryType::BOOT_SERVICES_CODE
+        | MemoryType::BOOT_SERVICES_DATA
+        | MemoryType::LOADER_CODE
+        | MemoryType::LOADER_DATA => MemoryKind::USABLE,
+
+        MemoryType::UNUSABLE => MemoryKind::UNUSABLE,
+
+        MemoryType::ACPI_RECLAIM => MemoryKind::ACPI_TABLES,
+
+        MemoryType::RUNTIME_SERVICES_CODE | MemoryType::RUNTIME_SERVICES_DATA => {
+            MemoryKind::FIRMWARE
+        }
+
+        _ => MemoryKind::RESERVED,
+    }
 }
---
+++ kernel/efiboot/src/setup.rs
@@ -0,0 +1,67 @@
+use core::mem::{self, MaybeUninit};
+
+use alloc::vec;
+use bootinfo::builder::Builder;
+use bootinfo::MemoryRange;
+use uefi::proto::fs::{OpenMode, SimpleFileSystem};
+use uefi::proto::image::LoadedImage;
+use uefi::table::{BootServices, BootTable};
+use uefi::{u16cstr, Handle, MemoryDescriptor, Result};
+use uninit::extension_traits::AsOut;
+
+use crate::{elfload, page};
+
+const BOOTINFO_FIXED_SIZE: usize = 0x1000;
+
+pub struct SetupCtx {
+    pub kernel_entry: usize,
+    pub mmap_buf: &'static mut [MaybeUninit<u8>],
+    pub bootinfo_builder: Builder<'static>,
+}
+
+pub fn setup(image_handle: Handle, boot_table: &BootTable) -> Result<SetupCtx> {
+    let boot_services = boot_table.boot_services();
+
+    let kernel_entry = load_kernel(image_handle, boot_services)?;
+    let mmap_size = boot_services.memory_map_size()? + 0x200;
+    let max_mmap_entries = mmap_size / mem::size_of::<MemoryDescriptor>();
+
+    let bootinfo_builder = make_bootinfo_builder(boot_services, max_mmap_entries)?;
+
+    Ok(SetupCtx {
+        kernel_entry: kernel_entry as usize,
+        mmap_buf: vec![MaybeUninit::uninit(); mmap_size].leak(),
+        bootinfo_builder,
+    })
+}
+
+fn load_kernel(image_handle: Handle, boot_services: &BootServices) -> Result<u64> {
+    let loaded_image = boot_services.open_protocol::<LoadedImage>(image_handle, image_handle)?;
+
+    let boot_fs = boot_services
+        .open_protocol::<SimpleFileSystem>(loaded_image.device_handle(), image_handle)?;
+
+    let root_dir = boot_fs.open_volume()?;
+    let mut file = root_dir.open(u16cstr!("regasos\\kernel"), OpenMode::READ)?;
+
+    elfload::load_elf(boot_services, &mut file)
+}
+
+fn make_bootinfo_builder(
+    boot_services: &BootServices,
+    max_mmap_entries: usize,
+) -> Result<Builder<'static>> {
+    let buf = alloc_uninit_pages(
+        boot_services,
+        BOOTINFO_FIXED_SIZE + max_mmap_entries / mem::size_of::<MemoryRange>(),
+    )?;
+    Ok(Builder::new(buf.as_out()).expect("buffer should be large and aligned"))
+}
+
+fn alloc_uninit_pages(
+    boot_services: &BootServices,
+    bytes: usize,
+) -> Result<&'static mut [MaybeUninit<u8>]> {
+    let p = page::alloc_pages(boot_services, bytes)?;
+    Ok(unsafe { &mut *(p.as_ptr() as *mut _) })
+}
