695f776c14dee8a5bf6744ef047b7d4672b5176a
Noam Raz <noamraz8@gmail.com>
2021-10-17 11:37:55 +0300

    Create kernel `arch` module

--- kernel/kernel/src/main.rs
+++ kernel/kernel/src/main.rs
@@ -1,7 +1,11 @@
+#![feature(asm, global_asm)]
+#![deny(unsafe_op_in_unsafe_fn)]
 #![no_std]
 #![no_main]
 
 use core::panic::PanicInfo;
+
+mod arch;
 
 #[panic_handler]
 fn handle_panic(_info: &PanicInfo) -> ! {
