f4e7285c5ee4a6fb5149ec4831991bd460504ab3
Noam Raz <noamraz8@gmail.com>
2021-10-18 09:31:05 +0300

    Create assembly macros for defining functions

--- kernel/kernel/src/arch/x86_64/boot.s
+++ kernel/kernel/src/arch/x86_64/boot.s
@@ -1,7 +1,21 @@
+.macro func name
+.type \name, @function
+\name:
+.endm
+
+.macro func_end name
+.size \name, . - \name
+.endm
+
+.macro global_func name
+.global \name
+func \name
+.endm
+
 .section .boottext, "ax"
 
-.global boot_main
-boot_main:
+global_func boot_main
 1:
     hlt
     jmp 1b
+func_end boot_main
