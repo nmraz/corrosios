dc0a46e576bd2c6e77ab655045a192989c5c35d0
Noam Raz <noamraz8@gmail.com>
2021-11-13 20:39:35 +0200

    Rename GDT flag

--- kernel/kernel/src/arch/x86_64/gdt.rs
+++ kernel/kernel/src/arch/x86_64/gdt.rs
@@ -5,7 +5,7 @@ bitflags! {
     pub struct DescriptorFlags: u64 {
         const WRITE = 1 << 41;
         const EXEC = 1 << 43;
-        const USER = 1 << 44;
+        const NON_SYSTEM = 1 << 44;
         const RING3 = 3 << 45;
         const PRESENT = 1 << 47;
         const LONG_MODE = 1 << 53;
