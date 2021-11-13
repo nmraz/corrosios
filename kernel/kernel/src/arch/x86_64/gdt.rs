7b294e545e3ea22bd3039a01a3bfced1dc978378
Noam Raz <noamraz8@gmail.com>
2021-11-13 20:51:10 +0200

    Create basic GDT

--- kernel/kernel/src/arch/x86_64/gdt.rs
+++ kernel/kernel/src/arch/x86_64/gdt.rs
@@ -11,3 +11,16 @@ bitflags! {
         const LONG_MODE = 1 << 53;
     }
 }
+
+#[no_mangle]
+static GDT: [DescriptorFlags; 2] = [
+    // Null descriptor
+    DescriptorFlags::empty(),
+    // Kernel code segment
+    DescriptorFlags::from_bits_truncate(
+        DescriptorFlags::NON_SYSTEM.bits()
+            | DescriptorFlags::PRESENT.bits()
+            | DescriptorFlags::EXEC.bits()
+            | DescriptorFlags::LONG_MODE.bits(),
+    ),
+];
