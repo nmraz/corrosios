7e898a223dc3b1bb39ff87f3812c67350144f991
Noam Raz <noamraz8@gmail.com>
2021-11-20 13:15:08 +0200

    Begin writing early boot code

--- kernel/kernel/src/arch/x86_64/boot.rs
+++ kernel/kernel/src/arch/x86_64/boot.rs
@@ -1,1 +1,7 @@
-global_asm!(include_str!("boot.s"));
+use super::gdt::GDT_SIZE;
+
+// Work around rustc bug that treats this constant as unused.
+#[allow(dead_code)]
+const KERNEL_OFFSET: u64 = 0xffffffff80000000;
+
+global_asm!(include_str!("boot.s"), GDT_SIZE = const GDT_SIZE, KERNEL_OFFSET = const KERNEL_OFFSET);
--- kernel/kernel/src/arch/x86_64/boot.s
+++ kernel/kernel/src/arch/x86_64/boot.s
@@ -1,21 +1,28 @@
-.macro func name
-.type \name, @function
-\name:
-.endm
+.bss
 
-.macro func_end name
-.size \name, . - \name
-.endm
+.set BOOT_STACK_SIZE, 0x4000 # 16K
+.set BOOT_STACK_ALIGN, 0x10 # As per ABI
 
-.macro global_func name
-.global \name
-func \name
-.endm
+.align BOOT_STACK_ALIGN
+.type boot_stack, @object
+boot_stack:
+    .skip BOOT_STACK_SIZE
+.size boot_stack, . - boot_stack
+
+.section .boot.rodata, "a"
+
+.type early_gdtr, @object
+early_gdtr:
+    .word {GDT_SIZE}
+    .long GDT - {KERNEL_OFFSET}
+.size early_gdtr, . - early_gdtr
 
 .section .boot.text, "ax"
 
-global_func boot_main
+.global boot_main
+.type boot_main, @function
+boot_main:
 1:
     hlt
     jmp 1b
-func_end boot_main
+.size boot_main, . - boot_main
