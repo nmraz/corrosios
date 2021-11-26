f39be012775c04036ccbd85b63eec3476a98a458
Noam Raz <noamraz8@gmail.com>
2021-11-27 01:46:24 +0200

    Create module for kernel page tables

--- kernel/kernel/src/arch/x86_64.rs
+++ kernel/kernel/src/arch/x86_64.rs
@@ -1,3 +1,4 @@
 mod boot;
 mod gdt;
+mod kernel_tables;
 mod mmu;
---
+++ kernel/kernel/src/arch/x86_64/kernel_tables.rs
@@ -0,0 +1,22 @@
+use super::mmu::{PageTable, LEVEL_SHIFT, PAGE_SHIFT};
+
+const MB: usize = 0x100000;
+
+const PT_RANGE: usize = 1 << (LEVEL_SHIFT + PAGE_SHIFT);
+
+// Note: keep in sync with linker script
+const KERNEL_MAX: usize = 8 * MB;
+
+const KERNEL_PT_COUNT: usize = KERNEL_MAX / PT_RANGE;
+
+#[no_mangle]
+static mut KERNEL_PML4: PageTable = PageTable::new();
+
+#[no_mangle]
+static mut KERNEL_PDPT: PageTable = PageTable::new();
+
+#[no_mangle]
+static mut KERNEL_PD: PageTable = PageTable::new();
+
+#[no_mangle]
+static mut KERNEL_PTS: [PageTable; KERNEL_PT_COUNT] = [PageTable::new(); KERNEL_PT_COUNT];
