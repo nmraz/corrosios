e79d61c5b505abc3c891837c8be92ebc4cecffbe
Noam Raz <noamraz8@gmail.com>
2022-08-19 18:04:35 +0200

    Increase maximum physmap size to 64TiB

--- kernel/kernel/src/arch/x86_64/kernel_vmspace.rs
+++ kernel/kernel/src/arch/x86_64/kernel_vmspace.rs
@@ -1,4 +1,6 @@
 use crate::mm::types::VirtPageNum;
 
 pub const PHYS_MAP_BASE: VirtPageNum = VirtPageNum::new(0xFFFF800000000);
-pub const PHYS_MAP_PAGES: usize = 0x1000000;
+
+// 64TiB
+pub const PHYS_MAP_PAGES: usize = 400000000;
