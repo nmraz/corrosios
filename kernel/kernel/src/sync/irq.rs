fec7035ebc7088c60cbd5d00af4b8ed9e8ccfcc9
Noam Raz <noamraz8@gmail.com>
2022-09-24 18:11:21 +0300

    Ensure that `IrqDisabled` token is `!Send`

--- kernel/kernel/src/sync/irq.rs
+++ kernel/kernel/src/sync/irq.rs
@@ -1,10 +1,14 @@
+use core::marker::PhantomData;
+
 use crate::arch;
 
 /// A type-level assertion that interrupts are disabled
 ///
 /// Whenever an instance of this structure is alive, users can safely assume that interrupts are
 /// disbled.
-pub struct IrqDisabled(());
+pub struct IrqDisabled {
+    _not_send: PhantomData<*const ()>,
+}
 
 impl IrqDisabled {
     /// # Safety
@@ -12,7 +16,9 @@ impl IrqDisabled {
     /// Interrupts must actually be disabled when this function is called and must remain disabled
     /// for the duration of the returned object's lifetime.
     pub unsafe fn new() -> Self {
-        Self(())
+        Self {
+            _not_send: PhantomData,
+        }
     }
 }
 
@@ -21,7 +27,7 @@ pub fn without<R>(f: impl FnOnce(&IrqDisabled) -> R) -> R {
         let prev_state = arch::cpu::irq_enabled();
         arch::cpu::disable_irq();
 
-        let ret = f(&IrqDisabled(()));
+        let ret = f(&IrqDisabled::new());
 
         if prev_state {
             arch::cpu::enable_irq();
