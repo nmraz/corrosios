ed7d57da480da8532d1e9f1d4290a51f0350cc04
Noam Raz <noamraz8@gmail.com>
2022-10-30 00:31:22 +0300

    Add simple framebuffer test

--- kernel/kernel/src/main.rs
+++ kernel/kernel/src/main.rs
@@ -9,10 +9,20 @@
 
 extern crate alloc;
 
+use core::{mem, slice};
+
 use arch::cpu;
+use bootinfo::item::Framebuffer;
+use bootinfo::view::View;
+use bootinfo::ItemKind;
 use log::{debug, info};
 use mm::types::PhysAddr;
+use num_utils::div_ceil;
 
+use crate::arch::mmu::PAGE_SIZE;
+use crate::mm::physmap::paddr_to_physmap;
+use crate::mm::types::{CacheMode, Protection};
+use crate::mm::vm::kernel_aspace::iomap;
 use crate::sync::irq::IrqDisabled;
 
 #[macro_use]
@@ -70,6 +80,55 @@ extern "C" fn kernel_main(
     debug!("triggering IRQ 55");
     unsafe {
         core::arch::asm!("int 55");
+    }
+
+    let bootinfo_slice =
+        unsafe { slice::from_raw_parts(paddr_to_physmap(bootinfo_paddr).as_ptr(), bootinfo_size) };
+
+    let bootinfo = View::new(bootinfo_slice).expect("bad bootinfo");
+
+    let framebuffer_item = bootinfo
+        .items()
+        .find(|item| item.kind() == ItemKind::FRAMEBUFFER)
+        .expect("no framebuffer");
+
+    let framebuffer_desc: &Framebuffer =
+        unsafe { framebuffer_item.get() }.expect("framebuffer info invalid");
+
+    let framebuffer_paddr = PhysAddr::new(framebuffer_desc.paddr);
+
+    debug!(
+        "framebuffer: phys range {}-{}, dimensions {}x{}, format {:?}",
+        framebuffer_paddr,
+        framebuffer_paddr + framebuffer_desc.size,
+        framebuffer_desc.width,
+        framebuffer_desc.height,
+        framebuffer_desc.format
+    );
+
+    let framebuffer_mapping = unsafe {
+        iomap(
+            framebuffer_paddr.containing_frame(),
+            div_ceil(framebuffer_desc.size, PAGE_SIZE),
+            Protection::READ | Protection::WRITE,
+            CacheMode::WriteCombining,
+        )
+    }
+    .expect("failed to map framebuffer");
+
+    debug!("framebuffer mapped at {}", framebuffer_mapping.addr());
+
+    let framebuffer_slice: &mut [u32] = unsafe {
+        slice::from_raw_parts_mut(
+            framebuffer_mapping.addr().as_mut_ptr(),
+            framebuffer_desc.size / mem::size_of::<u32>(),
+        )
+    };
+
+    for row in 0..framebuffer_desc.height {
+        for col in 0..framebuffer_desc.width {
+            framebuffer_slice[(row * framebuffer_desc.stride + col) as usize] = 0xff;
+        }
     }
 
     debug!("causing irrecoverable page fault");
